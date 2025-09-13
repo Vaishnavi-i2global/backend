9b774a21cad990b7f5c5e1552acbaf2838ee7759
Vaishnavi-i2global <vaishnavi@i2global.io>
2025-09-13 18:14:06 +0530

    changes

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate your virtual environment
-source ./myenv/bin/activate
+source ./venv/bin/activate
 
 
-./myenv/bin/gunicorn main:app --bind 0.0.0.0:8000
+./venv/bin/gunicorn main:app --bind 0.0.0.0:8000
